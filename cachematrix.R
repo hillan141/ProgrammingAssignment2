2ca8105f770d93351b3c40a8c1ab85ba2241fe8f
hillan141 <hillan141@gmail.com>
2015-01-24 12:03:16 -0500

    Functioning solution

--- cachematrix.R
+++ cachematrix.R
@@ -1,10 +1,21 @@
 ## Put comments here that give an overall description of what your
 ## functions do
 
-## Write a short comment describing this function
+## This function prepares a memoization object
+## for a matrix inverse
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  inv.x <- NULL
+  set <- function(y) {
+      x <<- y
+      inv.x <<- NULL
+  }
+  get <- function() x
+  setinv <- function(inverse.x) inv.x <<- inverse.x
+  getinv <- function() inv.x
+  list(set = set, get = get, 
+       setmean = setmean,
+       getmean = getmean)
 }
 
 
